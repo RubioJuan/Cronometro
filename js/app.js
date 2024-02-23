80c739801d3d14f1897ec3062d0296d300bc3e8d
Juan Felipe Rubio Sanabria <152001380+RubioJuan@users.noreply.github.com>
2024-02-23 18:32:19 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@ function iniciarCronometro() {
         cronometro.setInterval(actualizarTiempo, 1000);
     }
 }
-// // El setInterval llama a una función o ejecuta un fragmento de 
-// código de forma reiterada, con un retardo de tiempo fijo 
+// // El setInterval llama a una función o ejecuta un fragmento de código de forma 
+// reiterada, con un retardo de tiempo fijo 
 // entre cada llamada.
 
 function Pausa() {
@@ -23,7 +23,21 @@ function reiterarCronometro() {
     tiempo = 0;
     actualizarTiempo();
 }
+function actulizarTiempo() {
+    const horas = Math.floor(tiempo / 3600);
+    const minutos = Math.floor((tiempo % 3600) / 60);
+    const segundos = tiempo % 60;
 
+    const horasStr = horas < 10 ? "0" + horas : horas;
+    const minutosStr = minutos < 10 ? "0" + minutos : minutos;
+    const segundosStr = segundos < 10 ? "0" + segundos : segundos;
+
+    document.getElementById("cronometro").innerText = horasStr + ":" + minutosStr + ":" + segundosStr;
+}
+
+// Math.floor(x)
+// Devuelve el mayor entero menor que o igual a un número.
+// Lo utilice porque estamos utilizando la function para actualizar el tiempo and eso hace el codigo Math.floor
 
 // function actualizarCronometro() {
 //     tiempo++;
